0d16d4b21212224908bef8846ae517064203f50c
CheonpyoPark <1000pyoen@gmail.com>
2023-09-14 16:43:48 +0900

    TimesTable

---
+++ 20230914/20230914/Lecture10.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, b;
+    for (i = 2; i <= 9; i++) {
+        if (i == 5) continue;
+        for (b = 1; b <= 9; b++) {
+            printf("%d * %d = %d\n", i, b, i * b);
+
+
+        }
+    }
+    return 0;
+}
