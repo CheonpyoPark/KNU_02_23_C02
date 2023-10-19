ee86326a41780214b1ba9995ddf9002fe76bd815
CheonpyoPark <1000pyoen@gmail.com>
2023-10-19 15:14:03 +0900

    add_Stucture

--- 20231019/20231019/Lecture1.cpp
+++ 20231019/20231019/Lecture1.cpp
@@ -1,22 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    int sno;
+    char name[10];
+    int score;
+};
 
 int main(void) {
-	int STnum=NULL, count=0;
-	int* score;
-	score = (int*)malloc(sizeof(int)*STnum);
-	printf("학생수 입력:");
-	scanf_s("%d" , &STnum);
+    int STnum;
+    printf("학생수 입력: ");
+    scanf_s("%d", &STnum);
 
+    struct Student* std;
+    std = (struct Student*)malloc(sizeof(struct Student) * STnum);
 
-	for (count= 0;count<STnum;count++) {
-		printf("학생# %d-%d 성적입력: ",STnum, count + 1);
-		scanf_s("%d", score+count);
-	}
-	int sum = 0;
-	for (int count = 0;count < STnum;count++) {
-		sum += score[count];
-	}
-	printf("%d\n",sum);
+    for (int count = 0; count < STnum; count++) {
+        printf("학생# %d 학번입력: ", count + 1);
+        scanf_s("%d", &(std[count].sno));
 
+        printf("학생# %d 이름입력: ", count + 1);
+        scanf_s("%s",std[count].name,10);
+
+        printf("학생# %d 성적입력: ", count + 1);
+        scanf_s("%d", &(std[count].score));
+    }
+
+    int sum = 0;
+    for (int count = 0; count < STnum; count++) {
+        sum += std[count].score;
+    }
+    printf("전체 학생 성적의 합: %d\n", sum);
+
+    free(std);
+
+    return 0;
 }
